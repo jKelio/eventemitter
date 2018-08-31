1df109923e02f46f2d2f7438688f77e0c15a9576
Leon Jäkel <info@leon-jaekel.com>
2018-08-31 22:00:41 +0200

    Unit tests fixed cause of added Generic types
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   test/lib/event-emitter.factory.spec.ts
    	modified:   test/lib/event-emitter.spec.ts

--- test/lib/event-emitter.factory.spec.ts
+++ test/lib/event-emitter.factory.spec.ts
@@ -3,7 +3,7 @@ import { EventEmitterFactory, EventEmitter } from '../../src';
 
 describe('EventEmitterFactory', () => {
     it('should can create an instance of EventEmitter', () => {
-        const eventEmitter: EventEmitter = EventEmitterFactory.createEventEmitter();
+        const eventEmitter: EventEmitter<Function> = EventEmitterFactory.createEventEmitter();
         expect(eventEmitter).is.instanceof(EventEmitter);
     });
 });
--- test/lib/event-emitter.spec.ts
+++ test/lib/event-emitter.spec.ts
@@ -3,7 +3,7 @@ import { EventEmitter } from '../../src';
 
 describe('EventEmitter', () => {
     it('should be creatable', () => {
-        const eventEmitter: EventEmitter = new EventEmitter();
+        const eventEmitter: EventEmitter<Function> = new EventEmitter();
         expect(eventEmitter).is.instanceof(EventEmitter);
     });
 });
